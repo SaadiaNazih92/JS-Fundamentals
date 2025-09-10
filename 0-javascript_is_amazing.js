3d7012dc3f7750e56ff5b5e458aeb0cf92c7718a
Saadia Nazih <saadia.nazih92@gmail.com>
2025-09-10 18:31:49 +0200

    more changes

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -1,6 +1,3 @@
 const myVar = "JavaScript is amazing";
 console.log(myVar);
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
-console.log("hello js");
+
