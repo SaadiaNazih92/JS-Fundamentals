39b2e7c6dd32d8c1d75a44abeb8f7d2f459938d7
Saadia Nazih <saadia.nazih92@gmail.com>
2025-09-11 10:54:42 +0200

    update integer fil

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -5,5 +5,5 @@ process.argv[2]
 if (isNaN(numeroConvertito)){
     console.log("Not a number");
 }else{
-    console.log("My number:" + numeroConvertito)
+    console.log("My number: " + numeroConvertito)
 };
