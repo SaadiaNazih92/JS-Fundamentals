7b7900ffb4505e3adc0c1031ec11a0920b19b802
Saadia Nazih <saadia.nazih92@gmail.com>
2025-09-11 11:29:16 +0200

    update loops file

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-const nomarray = ["simo", "ousama", "mehdi"];
+const nomarray = ["C is fun", "Python is cool", "JavaScript is amazing"];
 for (let i = 0 ; i< nomarray.length ; i++){
 
     console.log(nomarray[i])
