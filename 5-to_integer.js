841d9be7070b35123aaee63be9f3331b2e67e1dd
Saadia Nazih <saadia.nazih92@gmail.com>
2025-09-11 09:09:08 +0200

    added 5-to_integer.js file

---
+++ 5-to_integer.js
@@ -0,0 +1,9 @@
+
+const numeroConvertito = parseInt(process.argv[2])
+process.argv[2]  
+
+if (isNaN(numeroConvertito)){
+    console.log("Not a number");
+}else{
+    console.log("My number:" + numeroConvertito)
+};
